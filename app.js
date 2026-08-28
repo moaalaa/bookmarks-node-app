c403fb5781ffaa9d9386003d60f1417fd232dab8
Mohamed Alaa El-Din <moaalaa16@gmail.com>
2026-08-28 21:25:21 +0300

    Adding Mogoos and redis

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const os = require('os');
+const mongoose = require('mongoose');
+const redis = require('redis');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoUrl = process.env.MONGO_URL;
+const redisUrl = process.env.REDIS_URL;
+
+const client = redis.createClient({
+    url: redisUrl,
+})
+    .on("error", (err) => console.log("Redis Client Error", err))
+    .connect()
+    .then(() => console.log('Redis Connected'));
+
+mongoose
+    .connect(mongoUrl)
+    .then(() => console.log('Mongo Connected'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'Hello, Worlds`!',
-        source: os.hostname()
+        message: 'Hello, World`!',
+        source: os.hostname(),
     });
 });
 
